abd4c4805f128196f2b83ad0be672544c998fb6a
Cepperaa <cepperaa@gmail.com>
2021-08-17 15:40:59 +0300

    task 3 fix

--- src/edu/homeWork/task3/Main.java
+++ src/edu/homeWork/task3/Main.java
@@ -4,13 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int x1 = 3;
-        int y1 = 3;
+        int x11 = 3;
+        int y11 = 3;
+        int x12 = 6;
+        int y12 = 5;
 
-        int x2 = 6;
-        int y2 = 5;
+        int x21 = 6;
+        int y21 = 5;
+        int x22 = 4;
+        int y22 = -1;
 
-        int scalar = x1 * x2 + y1 * y2;
+        int scalar = 2;
+
+        int x1 = x12 - x11;
+        int y1 = y12 - y11;
+        int x2 = x22 - x21;
+        int y2 = y22 - y21;
+
+        int product = x1 * x2 + y1 * y2;
 
         int x3 = scalar * x1;
         int y3 = scalar * y1;
@@ -18,16 +29,15 @@ public class Main {
         int x4 = scalar * x2;
         int y4 = scalar * y2;
 
-        System.out.println("Начало вектора (" + x3 + " " + y3 + "),  конец(" + x4 + " " + y4 + ")");
-        System.out.println("Длинна вектора 1 - " + vectorLength(x1, y1, x2, y2));
-        System.out.println("Длинна вектора 2 - " + vectorLength(x3, y3, x4, y4));
+        System.out.println("Вектор 1 * вектор 2 = " + product);
+        System.out.println("Вектор 1 * скаляр = {" + x3 + ", " + y3 + '}');
+        System.out.println("Вектор 2 * скаляр = {" + x4 + ", " + y4 + '}');
+        System.out.println("Длинна вектора 1 - " + vectorLength(x1, y1));
+        System.out.println("Длинна вектора 2 - " + vectorLength(x2, y2));
     }
 
-    public static double vectorLength(int x1, int y1, int x2, int y2) {
-        int x = x2 - x1;
-        int y = y2 - y1;
+    public static double vectorLength(int x, int y) {
         double solution = Math.pow(x, 2) + Math.pow(y, 2);
-        double result = Math.sqrt(solution);
-        return result;
+        return Math.sqrt(solution);
     }
 }
