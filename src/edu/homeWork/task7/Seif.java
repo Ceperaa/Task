41421daac29677b58b97cdbf2c4acb703c03d765
timmeleshko <84080950+timmeleshko@users.noreply.github.com>
2021-08-19 01:28:53 +0300

    Update Seif.java

--- src/edu/homeWork/task7/Seif.java
+++ src/edu/homeWork/task7/Seif.java
@@ -3,8 +3,9 @@ package edu.homeWork.task7;
 public class Seif {
 
     private final DragMetal dragMetal; // Опять же... + по условию мы набиваем сейф и закрываем его. Не уверен, но возможно, его уже нельзя открыть и что-то изменить там. Значит Final.
-
-
+    // PS: после ревью Main.java. Здесь будет поле private final List<DragMetal> dragMetalList;
+    
+    
     public Seif(DragMetal dragMetal) {
         this.dragMetal = dragMetal;
     }
