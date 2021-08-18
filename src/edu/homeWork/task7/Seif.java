5d6bee3e67834adc82965e46308c06dff416cd87
timmeleshko <84080950+timmeleshko@users.noreply.github.com>
2021-08-19 01:20:14 +0300

    Review task7 - part 2.

--- src/edu/homeWork/task7/Seif.java
+++ src/edu/homeWork/task7/Seif.java
@@ -2,19 +2,14 @@ package edu.homeWork.task7;
 
 public class Seif {
 
-    DragMetal dragMetal;
+    private final DragMetal dragMetal; // Опять же... + по условию мы набиваем сейф и закрываем его. Не уверен, но возможно, его уже нельзя открыть и что-то изменить там. Значит Final.
 
 
     public Seif(DragMetal dragMetal) {
-
         this.dragMetal = dragMetal;
     }
 
     public DragMetal getDragMetal() {
         return dragMetal;
-    }
-
-    public void setDragMetal(DragMetal dragMetal) {
-        this.dragMetal = dragMetal;
     }
 }
