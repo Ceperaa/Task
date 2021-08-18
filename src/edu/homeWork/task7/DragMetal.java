7adbe68a4d4e754a6f00039346d3af10303fcb17
timmeleshko <84080950+timmeleshko@users.noreply.github.com>
2021-08-19 01:15:14 +0300

    Review task7 - part1.

--- src/edu/homeWork/task7/DragMetal.java
+++ src/edu/homeWork/task7/DragMetal.java
@@ -1,20 +1,28 @@
 package edu.homeWork.task7;
 
 public class DragMetal {
-    int price;
-    int amount;
-    String name;
-    int id;
-
-
+    
+    private int price; // Никогда не оставляй прямой доступ к полям pojo / beans классам. На это дело есть геттеры /сеттеры.
+    private int amount;
+    private String name;
+    private int id; // Если мы знаем, что поле не изменится - добавляем модификатор final.
 
     public DragMetal(int price, int amount, String name) {
         this.price = price;
         this.amount = amount;
         this.name = name;
-
     }
 
+    public int getPrice() {
+        return price;
+    }
+    
+    public void setPrice(int price) { // Если поле требуется мутабельное.
+        this.price = price;
+    }
+    
+    ...
+    
     public int getId() {
         return 0;
     }
